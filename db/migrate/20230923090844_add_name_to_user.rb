18a7c66d5bb6f12a066362c0004388b179067e15
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-23 17:48:16 +0800

    Add name to User

---
+++ db/migrate/20230923090844_add_name_to_user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNameToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+
+    add_index :users, :name, unique: true
+  end
+end
