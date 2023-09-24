f26ec778ebaec0d7c56ad5570423c1c56ddffd1d
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-24 10:18:45 +0800

    set rubocop to prefer compact style for class and module children

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsBookingSystem
+module RailsBookingSystem # rubocop:disable Style/ClassAndModuleChildren
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
