24f697339fd2dce73e18dfb4d8524b3b53da4b59
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-27 18:46:01 +0800

    add api to fetch all rooms

---
+++ app/controllers/api/v1/rooms_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Api::V1::RoomsController < ::Api::ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @rooms = Room.all
+    @rooms = @rooms.where('capacity >= ?', params[:capacity]) if params[:capacity]
+    @rooms = @rooms.where('? = ANY(tags)', tags) if tags.present?
+
+    render json: @rooms, each_serializer: RoomSerializer
+  end
+
+  private
+
+  def tags
+    @tags ||= (params[:tags].is_a?(Array) ? params[:tags] : [params[:tags]]).compact
+  end
+end
--- db/migrate/20230927103228_change_tags_to_room.rb
+++ db/migrate/20230927103228_change_tags_to_room.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ChangeTagsToRoom < ActiveRecord::Migration[7.0]
+  # rubocop:disable Rails/BulkChangeTable
   def up
     remove_column :rooms, :tags
     add_column :rooms, :tags, :string, array: true, default: []
@@ -10,4 +11,5 @@ class ChangeTagsToRoom < ActiveRecord::Migration[7.0]
     remove_column :rooms, :tags
     add_column :rooms, :tags, :string, array: false, default: nil
   end
+  # rubocop:enable Rails/BulkChangeTable
 end
