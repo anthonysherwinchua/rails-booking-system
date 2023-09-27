fc8b7585b909a41598dc320c80df5594268a03ce
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-27 15:02:23 +0800

    refactor NavBar location

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import Routes from "../routes";
+import NavBar from "../components/views/common/NavBar";
 
-export default props => <>{Routes}</>;
+export default props => (
+  <>
+    <NavBar />
+    {Routes}
+  </>
+);
