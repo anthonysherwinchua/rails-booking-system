32c7b561d0a8d231b8d20d502f95a1ffb9a2f468
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-28 11:37:16 +0800

    add more validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,7 +6,11 @@ class Booking < ApplicationRecord
 
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validate :validate_time_increment, :validate_no_overlapping_booking
+  validate :validate_booking_time, :prevent_update_for_past_bookings, :validate_time_increment,
+           :validate_no_overlapping_booking
+
+  scope :future_dated, -> { where('start_time > ?', Time.zone.now) }
+  scope :past, -> { where('end_time < ?', Time.zone.now) }
 
   private
 
@@ -26,5 +30,21 @@ class Booking < ApplicationRecord
                                   .where.not(id: id)
                                   .where('start_time < ? AND end_time > ?', end_time, start_time)
     errors.add(:base, 'Booking overlaps with existing booking') if overlapping_bookings.exists?
+  end
+
+  def validate_booking_time
+    return unless start_time
+
+    return unless start_time < 1.minute.ago
+
+    errors.add(:start_time, 'cannot be in the past')
+  end
+
+  def prevent_update_for_past_bookings
+    return unless persisted?
+
+    return unless (start_time_changed? || end_time_changed?) && start_time_was < Time.zone.now
+
+    errors.add(:base, 'Past bookings cannot be updated')
   end
 end
